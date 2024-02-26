b774ffa898ce02c17262a26887e8e828ef1ff6bd
Yevhen-L <ujeen2101@gmail.com>
2024-02-26 19:45:20 +0200

    0

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Button } from 'components/Botton';
+
+export const App = () => {
+  return (
+    <div>
+      <Button />
+
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Botton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './button.module.css';
+
+export const Button = props => {
+  return <span className={styles.box}>Text</span>;
+};
